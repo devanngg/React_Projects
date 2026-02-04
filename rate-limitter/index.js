1aa066d244b9f2b9bcce999a40d290a49af627ef
devangg <devangsabbarwal46509@gmail.com>
2026-02-04 12:52:09 +0530

    Added multi pipeline to reduce latency of rerquest

--- rate-limitter/index.js
+++ rate-limitter/index.js
@@ -9,46 +9,42 @@ const redis = require("./helpers/redis")
 const MAX_ALLOWED_REQUEST = 5;
 const MAX_TIME = 30; // seconds for Redis TTL
 
-app.use(async function(req, res, next) {
-try {
-const clientIp = req.ip||"127.0.0.1"
+app.use(async function (req,res,next){
+    try {
+const clientIp = req.ip ||ip.address();
 const now = Date.now();
 const windowStart = now-(MAX_TIME*1000)
-// Removing the time stamps that are older than 10seconds
-await redis.zremrangebyscore(clientIp,0,windowStart);
-const requestCount = await redis.zcard(clientIp);
+
+// THE PIPELINE 
+const pipeline = redis.multi();
+pipeline.zremrangebyscore(clientIp,0,windowStart)
+pipeline.zcard(clientIp)
+pipeline.expire(clientIp,MAX_TIME);
+
+const results = await pipeline.exec();
+const requestCount =  results[1][1]
 
 if(requestCount>=MAX_ALLOWED_REQUEST){
-    console.log(`!!![SLIDING BLOCK] ${clientIp} is too fast`)
-    return res.status(429).json({message:"Too many request -Sliding window black"})
+    console.log(`BLOCKED ${clientIp} is doing too much.Go touch grass`)
+    return res.status(429).json({
+        error:"Rate limit exceededd",
+        msg:"Either you are trying to mess around or a no lifer"
+    })
 }
-// adds the current request timestamp to the set
 await redis.zadd(clientIp,now,now);
-// expire if the users leaves
-await redis.expire(clientIp,MAX_TIME);
+console.log(`${clientIp} passed. Count:${requestCount +1}/5`)
+next();
 
-console.log(`SLIDING REQUEST ${requestCount+1} for ${clientIp}`)
-next();
-}
-catch(err) {
-console.log("Reddis Sliding error",err.message)
-next();
     }
-});
-
+    catch(err) {
+console.log("either redis has dies mid way or even idk what is woring ")
+    }
+})
 app.get("/",function(req,res){
     console.log("Received a request!")
     res.status(200).send("Ok")
 })
 
-
 app.listen(3000,()=>console.log("Server is running on port 3000"))
 
 
-//We would be using redis for it 
-/// It is stored in memory
-// redis gives time to live 
-
-// A BIT ON THE HIGHER DIFFICULT SIDE ?
-
- 
